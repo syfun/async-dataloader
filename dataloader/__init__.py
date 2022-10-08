348583d5845e89264b146a9822099db47c095d9a
孙雨 <y.sun@teletraan.io>
2022-10-08 15:37:15 +0800

    chore: bump version 0.2.2

--- dataloader/__init__.py
+++ dataloader/__init__.py
@@ -3,7 +3,7 @@ __all__ = [
     "sort",
     "group_sort",
 ]
-__version__ = "0.2.1"
+__version__ = "0.2.2"
 
 from .dataloader import DataLoader  # noqa
 from .utils import sort, group_sort  # noqa
